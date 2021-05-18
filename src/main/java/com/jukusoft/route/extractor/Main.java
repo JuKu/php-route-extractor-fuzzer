637edc34b5d71988b564817ed2a67a626781095f
JuKu <kuenzel.justin@t-online.de>
2021-05-18 20:38:02 +0200

    Change SourceCodeParser

--- src/main/java/com/jukusoft/route/extractor/Main.java
+++ src/main/java/com/jukusoft/route/extractor/Main.java
@@ -1,7 +1,12 @@
 package com.jukusoft.route.extractor;
 
 import com.jukusoft.route.extractor.cli.CLIArgumentsParser;
+import com.jukusoft.route.extractor.parser.Route;
+import com.jukusoft.route.extractor.parser.SourceCodeParser;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,9 +18,16 @@ public class Main {
      * main method
      * @param args
      */
-    public static void main(String[] args) {
-        //parse CLI arguments
-        Map<String,String> params = CLIArgumentsParser.parseArguments(args);
+    public static void main(String[] args) throws IOException {
+        try {
+            //parse CLI arguments
+            Map<String,String> params = CLIArgumentsParser.parseArguments(args);
+
+            List<Route> routes = SourceCodeParser.parseSourceCodeDir(new File(params.get("src")));
+        } catch (IllegalArgumentException e) {
+            //logging is already done by CLIArgumentsParser
+            System.exit(1);
+        }
     }
 
 }
--- src/main/java/com/jukusoft/route/extractor/parser/SourceCodeParser.java
+++ src/main/java/com/jukusoft/route/extractor/parser/SourceCodeParser.java
@@ -1,7 +1,11 @@
 package com.jukusoft.route.extractor.parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +20,8 @@ import java.util.stream.Stream;
  */
 public class SourceCodeParser {
 
+    private static final Logger logger = LoggerFactory.getLogger(SourceCodeParser.class);
+
     /**
      * private constructor, because this class is a utility class
      */
@@ -24,22 +30,33 @@ public class SourceCodeParser {
     }
 
     public static List<Route> parseSourceCodeDir(File srcDir) throws IOException {
-        if (!srcDir.exists() || srcDir.isDirectory()) {
+        if (!srcDir.exists() || !srcDir.isDirectory()) {
             throw new IllegalArgumentException("src directory does not exists or is not a directory: " + srcDir.getAbsolutePath());
         }
 
+        logger.info("parse source directory: {}", srcDir.getAbsolutePath());
+
         List<Route> routes = Files.walk(Paths.get(srcDir.toURI()))
                 .filter(Files::isRegularFile)
-                .filter(file -> file.endsWith(".php"))
-                .map(file -> parseSourceCodeFile(file.toFile()))
+                .filter(file -> file.toFile().getAbsolutePath().endsWith(".php"))
+                .map(file -> parseSourceCodeFile(file))
                 .flatMap(list -> list.stream())
                 .collect(Collectors.toList());
 
         return routes;
     }
 
-    public static List<Route> parseSourceCodeFile(File file) {
+    public static List<Route> parseSourceCodeFile(Path path) {
         List<Route> routes = new ArrayList<>();
+
+        logger.info("parse file: {}", path.toFile().getAbsolutePath());
+
+        try {
+            Files.readString(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("ERROR - Cannot parse file: " + path.toFile().getAbsolutePath() + " because of exception: " + e.getLocalizedMessage());
+        }
 
         return routes;
     }
